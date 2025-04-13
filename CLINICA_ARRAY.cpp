c8766aefa345364c550c9a2aba13d0223d3d1ec9
WesleyLaura <wrlaurac@unjbg.edu.pe>
2025-04-13 00:47:22 -0500

    Agregr un programa de gestion de datos de un paciente de una clinica

---
+++ CLINICA_ARRAY.cpp
@@ -0,0 +1,205 @@
+//array ordenados y desordenados de un historial clinico de un paciente
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#define MAX 2
+using namespace std;
+
+struct paciente{
+	char hc[15],nomb[20];
+	float peso,talla,imc;
+};
+
+void menuDesordenados(paciente *, int &);
+//void menuOrdenados();
+void insertar_d(paciente *, int &);
+void eliminar_d(paciente *,int &);
+void modificar_d(paciente *, int &);
+void mostrar(paciente *, int);
+int main(){
+	paciente x[MAX];
+	int op,n;
+	n=-1;
+	do{
+		cout<<"----MENU-----\n";
+		cout<<"1.- Array DESORDENADOS\n";
+		cout<<"2.- Array ORDENADOS\n";
+		cout<<"3.- S A L I R\n";
+		cout<<"Ingrese la opcion: "; cin>>op;
+		switch(op){
+			case 1:
+				menuDesordenados(x,n);
+				break;
+			case 2:
+				//menuOrdenados();
+				break;
+			case 3:
+				break;		
+			
+		}
+	}while(op>0 && op<3);
+	
+	return 0;
+}
+
+void menuDesordenados(paciente *x, int &n){
+	system("cls");
+int op;
+	do{
+		cout<<"-----MENU DE ARRAY DESORDENADOS\n";
+		cout<<"1.- INSERTAR\n";
+		cout<<"2.- ELIMINAR\n";
+		cout<<"3.- MODIFICAR\n";
+		cout<<"4.- MOSTRAR\n";
+		cout<<"5.- REGRESAR AL MENU PRINCIPAL\n";
+		cout<<"Ingrese la opcion :"; cin>>op;
+		switch(op){
+			
+			case 1:
+				insertar_d(x,n);
+			    mostrar(x,n);
+				break;
+			case 2:
+				eliminar_d(x,n);
+				mostrar(x,n);
+				break;
+			case 3:
+				modificar_d(x,n);
+				mostrar(x,n);
+				break;
+			case 4:
+				mostrar(x,n);
+				break;
+			case 5:
+				break;				
+		}
+		
+	}while(op>0 && op<5);
+}
+
+void insertar_d(paciente *x,int &n){
+	int i,cen;
+	char hclinica[10];
+	
+	if(n<MAX-1){
+		cen=0;
+		i=0;
+		cout<<" HISTORIA CLINICA :";cin>>hclinica;
+		if(n>-1){
+			while(i<=n && (cen==0)){
+				if(strcmp(x[i].hc,hclinica)==0){
+					cen=1;
+				}else{
+					i++;
+				}	
+			}
+		}
+			if(cen==0){	
+			n++;
+			strcpy(x[i].hc,hclinica);
+			cout<<"NOMBRE: ";cin>>x[i].nomb;
+			cout<<"PESO: ";cin>>x[i].peso;
+			cout<<"TALLA :";cin>>x[i].talla;
+			x[i].imc=(x[i].peso/(x[i].talla*x[i].talla));
+			cout<<"INDICE DE MASA CORPORAL :"<<x[i].imc;
+			}else{
+				cout<<"DATO YA REGISTRADO EN LA BASE DE DATOS. No se realizo la insercion...\n";
+			}
+	}else{
+		cout<<"ARRAY LLENO, PELIGRO DE DESBORDAMIENTO...no se inserto dato\n";
+	}
+	cout<<"\n";
+}
+
+void eliminar_d(paciente *x, int &n){
+	int i,j,cen;
+	char hclinica[10];
+	if(n>-1){
+		cen=0;
+		i=0; 
+		cout<<"\nEscriba LA HISTORIA CLINICA que desea eliminar: ";cin>>hclinica;
+		while((i<=n)&& (cen==0)){
+			if(strcmp(hclinica,x[i].hc)==0){
+				n--;
+				for(j=i;j<=n;j++){
+					x[j]=x[j+1];
+				}
+				cen=1;
+			}else{
+				i++;
+			}
+		}
+		cout<<"\nDATO ELIMINADO EXITOSAMENTEE!!!!";
+			if(cen==0){
+				cout<<"\n---------------------------------------------\n";
+				cout<<"\n|"<<hclinica<<" NO EXISTE EN LA BASE DE DATOS|\n";
+				cout<<"\n---------------------------------------------\n";
+			}
+	}else{
+		cout<<"ARRAY VACIO...No se ha eliminado nada\n";
+	}
+	
+}
+
+void modificar_d(paciente *x, int &n){
+	int i,cen,op;
+	char hc1[10];
+	cout<<"\n\nCODIGO DE REGISTRO A MODIFICAR\n\n";
+	cout<<"INGRESE LA HISTORIA CLINICA: "; cin>>hc1;
+	if(n>-1){
+		i=0;
+		cen=0;
+		while(i<=n && (cen==0)){
+				if(strcmp(x[i].hc,hc1)==0){
+					cen=1;
+					cout<<"\n\nQUE CAMPO DESEA MODIFICAR\n\n";
+					cout<<"\n-------------------------------------------------------\n";
+					cout<<"|1.-HISTORIA CLINICA   2.-NOMBRE   3.-PESO   4.-TALLA    |";
+					cout<<"\n-------------------------------------------------------\n";
+					cout<<"OPCION: ";cin>>op;
+					switch(op){
+						
+						case 1:
+							cout<<"Ingrese la HISTORIA CLINICA CORRECTA: ";cin>>x[i].hc;
+							break;
+						case 2:
+							cout<<"Ingrese el NOMBRE CORRECTO: "; cin>>x[i].nomb;
+							break;
+						case 3:
+							cout<<"Ingrese el PESO CORRECTO: "; cin>>x[i].peso;
+							x[i].imc=(x[i].peso/(x[i].talla*x[i].talla));
+							break;
+						case 4:
+							cout<<"Ingrese la TALLA CORRECTA: "; cin>>x[i].talla;
+							x[i].imc=(x[i].peso/(x[i].talla*x[i].talla));
+							break;	
+					}
+				}else{
+					i++;
+				}	
+			}
+				if(cen==0){
+				cout<<"\n---------------------------------------------------------\n";
+				cout<<"|"<<hc1<<" DATO A MODIFICAR NO EXISTE EN LA BASE DE DATOS  |";
+				cout<<"\n---------------------------------------------------------\n";
+				}
+		
+	}else{
+		cout<<"ARREGLO VACIOOOO!!!";
+		system("pause");
+	}
+	
+}
+
+void mostrar(paciente *x,int n){
+	for(int i=0;i<=n;i++){
+		cout<<"\n\n["<<i+1<<"] : ";
+		cout<<"\nHISTORIA CLINICA : "<<x[i].hc;
+		cout<<"\nNOMBRE : "<<x[i].nomb;
+		cout<<"\nPESO: "<<x[i].peso;
+		cout<<"\nTALLA : "<<x[i].talla;
+		cout<<"\nINDICE DE MASA CORPORAL: "<<x[i].imc;	
+	cout<<"\n------------------------------------\n\n";
+	}
+	system("pause");
+}
